a94d5b7e1c4ef249169d515c31d100a69b6dcb4e
MinhThien-Luu <21521460@gm.uit.edu.vn>
2024-03-15 22:23:08 +0700

    first commit

---
+++ src/main/java/com/th1enlm02/studentsystem/service/StudentService.java
@@ -0,0 +1,10 @@
+package com.th1enlm02.studentsystem.service;
+
+import com.th1enlm02.studentsystem.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+}
